b1fb87484781e3cf1b5e95372f7e2bf1f74eaaac
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2022-12-16 14:52:48 +0700

    Fist Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Confetti from "react-confetti";
+import Die from "./components/Die";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+  // Create random number 1 to 6
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+  console.log(allNewDice());
+
+  const diceElements = dice.map((die) => <Die value={die} />);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="dice-container">{diceElements}</div>
+    </main>
   );
 }
-
-export default App;
